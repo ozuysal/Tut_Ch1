d46d4a86c19fd6a6cbe2f19c280ffa65ac42c300
Yusuf <ozuysal@stanford.edu>
2011-03-24 18:03:50 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TutCh1::Application.config.secret_token = 'a68f7d35bfadd91c82042dce3d17a3fb92ed66492ee02139b7cb95d13907fd4115125acd26b458b9069ba298dbbb0360c423c618a81faa8800d29d232ee31320'
